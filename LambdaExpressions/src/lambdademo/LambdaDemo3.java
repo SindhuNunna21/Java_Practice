93472e6c8537110c15a6ca79f73ed406333e8cff
SindhuNunna21 <138240108+SindhuNunna21@users.noreply.github.com>
2023-08-17 13:55:32 +0530

    Add files via upload

---
+++ LambdaExpressions/src/lambdademo/LambdaDemo3.java
@@ -0,0 +1,30 @@
+package lambdademo;
+
+@FunctionalInterface
+interface MyLambdaEx {
+	public void display();
+}
+
+/*
+ * class My implements MyLambda { public void display() {
+ * System.out.println("Hello World"); } }
+ */
+public class LambdaDemo3 {
+
+	public static void main(String[] args) {
+		MyLambda m = () -> {
+			System.out.println("Hello world");
+		};
+
+		/*
+		 * //MyLambda m=new My(); MyLambda m=//new MyLambda() ()->
+		 * 
+		 * //public void display() { System.out.println("Hello World");
+		 * 
+		 * };
+		 */
+		m.display();
+
+	}
+
+}
