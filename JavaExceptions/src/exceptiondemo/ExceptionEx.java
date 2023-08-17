8cef567c7446705f808cdaa1444fda2b350585ad
SindhuNunna21 <138240108+SindhuNunna21@users.noreply.github.com>
2023-08-17 13:46:00 +0530

    Add files via upload

---
+++ JavaExceptions/src/exceptiondemo/ExceptionEx.java
@@ -0,0 +1,26 @@
+package exceptiondemo;
+
+import java.util.Scanner;
+
+public class ExceptionEx {
+
+	public static void main(String[] args) {
+		int a, b, c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		try {
+			c = a / b;
+			System.out.println("Division is " + c);
+		}
+
+		catch (ArithmeticException e) {
+			System.out.println("Denominator should not be zero, try again");
+		}
+		System.out.println("Bye");
+
+	}
+
+}
